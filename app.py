c8764682f86a362f9399146655ad44c0d4e500b3
Vihanga Hemachandra <vihangamax997@gmail.com>
2024-12-14 11:07:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI()
+
+OOPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
+while True:
+    question = input("Ask a question: ")
+
+    if (question != "bye"):
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "system", "content": question}],
+            max_tokens=50,
+            n=1,
+            temperature=0
+            
+        )
+
+        for choice in response.choices:
+            print(f"AI: {choice.message.content}") 
+    else:
+        print("AI: Goodbye!")
+        break 
+        # print(response)
